10d30521a425bacab997850222f2c767ff4015a6
Yvan Gauthier <y.gauthier04@icloud.com>
2024-08-02 16:21:39 +0200

    initial commit

--- CoreMLDemo/ViewController.swift
+++ CoreMLDemo/ViewController.swift
@@ -5,15 +5,42 @@
 //  Created by Yvan Gauthier on 02/08/2024.
 //
 
+import CoreML
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(label)
+        view.addSubview(imageView)
     }
-
+    private func analyzeImage(image: UIImage?){
+       guard let buffer = image?.resize(size: CGSize(width: 224, height: 224))?
+        .getCVPixelBuffer()else{
+           return
+       }
+        
+        do{
+            let config = MLModelConfiguration()
+            let model = try GoogLeNetPlaces(configuration: config)
+            let input = GoogLeNetPlacesInput(sceneImage: buffer)
+        }
+        catch {
+            
+        }
+    }
 
 }
 
